660f1d7984f86a095f0635cb28c03cffa35da825
Konstantin Bukin <bukin@inetstudio.ru>
2017-08-28 11:23:14 +0300

    Views refactoring

--- resources/views/admin/pages/products/form.blade.php
+++ resources/views/admin/pages/products/form.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 
     @push('breadcrumbs')
-        @include('admin.module.products::partials.breadcrumbs')
+        @include('admin.module.products::partials.products.breadcrumbs')
         <li>
             <a href="{{ route('back.products.index') }}">Продукты</a>
         </li>
--- resources/views/admin/pages/products/index.blade.php
+++ resources/views/admin/pages/products/index.blade.php
@@ -14,7 +14,7 @@
 @section('content')
 
     @push('breadcrumbs')
-        @include('admin.module.products::partials.breadcrumbs')
+        @include('admin.module.products::partials.products.breadcrumbs')
     @endpush
 
     <div class="wrapper wrapper-content">
