e5c464ad76cfd052abdb36bb5b67fb99b6a6ba3a
Konstantin Bukin <bukin@inetstudio.ru>
2018-01-09 16:22:42 +0300

    Fixes

--- src/Console/Commands/ProcessGoogleFeeds.php
+++ src/Console/Commands/ProcessGoogleFeeds.php
@@ -35,82 +35,90 @@ class ProcessGoogleFeeds extends Command
         $tempPath = Storage::disk('temp')->getDriver()->getAdapter()->getPathPrefix();
 
         if (config('products.feeds.google')) {
-            foreach (config('products.feeds.google') as $url) {
+            foreach (config('products.feeds.google') as $productsBrand => $url) {
+                $this->info('Обработка бренда: '.$productsBrand);
+
                 $feedHash = md5($url);
 
                 $context = stream_context_create(['http' => ['header' => 'Accept: application/xml']]);
-                $contents = file_get_contents($url, false, $context);
-                $xml = simplexml_load_string($contents);
 
-                $products = [];
+                try {
+                    $contents = file_get_contents($url, false, $context);
+                } catch (\Exception $e) {
+                    $this->error('Фид недоступен: '.$url);
+                } finally {
+                    $xml = simplexml_load_string($contents);
 
-                foreach ($xml->channel->item as $item) {
-                    $product = $item->children('g', true);
+                    $products = [];
 
-                    $products[] = trim($product->id);
+                    foreach ($xml->channel->item as $item) {
+                        $product = $item->children('g', true);
 
-                    $productObj = ProductModel::updateOrCreate([
-                        'feed_hash' => $feedHash,
-                        'g_id' => trim($product->id),
-                    ], [
-                        'title' => (isset($product->title)) ? trim($product->title) : ((isset($item->title)) ? trim($item->title) : ''),
-                        'description' => (isset($product->description)) ? trim($product->description) : ((isset($item->description)) ? trim($item->description) : ''),
-                        'price' => (isset($product->price)) ? trim($product->price) : ((isset($item->price)) ? trim($item->price) : ''),
-                        'condition' => (isset($product->condition)) ? trim($product->condition) : ((isset($item->condition)) ? trim($item->condition) : ''),
-                        'availability' => (isset($product->availability)) ? trim($product->availability) : ((isset($item->availability)) ? trim($item->availability) : ''),
-                        'brand' => (isset($product->brand)) ? trim($product->brand) : ((isset($item->brand)) ? trim($item->brand) : ''),
-                        'product_type' => (isset($product->product_type)) ? trim($product->product_type) : ((isset($item->product_type)) ? trim($item->product_type) : ''),
-                    ]);
+                        $products[] = trim($product->id);
 
-                    $imageLink = (isset($product->image_link)) ? trim($product->image_link) : ((isset($item->image_link)) ? trim($item->image_link) : '');
-                    if ($imageLink) {
-                        if (! $productObj->hasMedia('preview')) {
-                            $tempFile = $tempPath.'/'.basename($imageLink);
+                        $productObj = ProductModel::updateOrCreate([
+                            'feed_hash' => $feedHash,
+                            'g_id' => trim($product->id),
+                        ], [
+                            'title' => (isset($product->title)) ? trim($product->title) : ((isset($item->title)) ? trim($item->title) : ''),
+                            'description' => (isset($product->description)) ? trim($product->description) : ((isset($item->description)) ? trim($item->description) : ''),
+                            'price' => (isset($product->price)) ? trim($product->price) : ((isset($item->price)) ? trim($item->price) : ''),
+                            'condition' => (isset($product->condition)) ? trim($product->condition) : ((isset($item->condition)) ? trim($item->condition) : ''),
+                            'availability' => (isset($product->availability)) ? trim($product->availability) : ((isset($item->availability)) ? trim($item->availability) : ''),
+                            'brand' => (isset($product->brand)) ? trim($product->brand) : ((isset($item->brand)) ? trim($item->brand) : ''),
+                            'product_type' => (isset($product->product_type)) ? trim($product->product_type) : ((isset($item->product_type)) ? trim($item->product_type) : ''),
+                        ]);
 
-                            $this->grabImage($imageLink, $tempFile);
+                        $imageLink = (isset($product->image_link)) ? trim($product->image_link) : ((isset($item->image_link)) ? trim($item->image_link) : '');
+                        if ($imageLink) {
+                            if (! $productObj->hasMedia('preview')) {
+                                $tempFile = $tempPath.'/'.basename($imageLink);
 
-                            $media = $productObj
-                                ->addMedia($tempFile)
-                                ->withCustomProperties(['source' => $imageLink])
-                                ->toMediaCollection('preview', 'products');
+                                $this->grabImage($imageLink, $tempFile);
 
-                            $media->custom_properties = [
-                                'processed' => true,
-                                'source' => $imageLink,
-                            ];
-                            $media->save();
+                                $media = $productObj
+                                    ->addMedia($tempFile)
+                                    ->withCustomProperties(['source' => $imageLink])
+                                    ->toMediaCollection('preview', 'products');
 
-                            event(new UpdateImageEvent($productObj, 'preview'));
-                        } else {
-                            if (! $productObj->getFirstMedia('preview')->hasCustomProperty('processed')) {
-                                $productObj->clearMediaCollection('preview');
-                                continue;
+                                $media->custom_properties = [
+                                    'processed' => true,
+                                    'source' => $imageLink,
+                                ];
+                                $media->save();
+
+                                event(new UpdateImageEvent($productObj, 'preview'));
+                            } else {
+                                if (! $productObj->getFirstMedia('preview')->hasCustomProperty('processed')) {
+                                    $productObj->clearMediaCollection('preview');
+                                    continue;
+                                }
+                            }
+                        }
+
+                        if (isset($product->link) or isset($item->link)) {
+                            $productLink = (isset($product->link)) ? $product->link : ((isset($item->link)) ? $item->link : '');
+                            if ($productLink) {
+                                ProductLinkModel::updateOrCreate([
+                                    'product_id' => $productObj->id,
+                                ], [
+                                    'link' => trim($productLink),
+                                ]);
+                            }
+                        } elseif (isset($product->links)) {
+                            ProductLinkModel::where('product_id', $productObj->id)->forceDelete();
+
+                            foreach ($product->links->link as $link) {
+                                ProductLinkModel::create([
+                                    'product_id' => $productObj->id,
+                                    'link' => trim($link->href),
+                                ]);
                             }
                         }
                     }
 
-                    if (isset($product->link) or isset($item->link)) {
-                        $productLink = (isset($product->link)) ? $product->link : ((isset($item->link)) ? $item->link : '');
-                        if ($productLink) {
-                            ProductLinkModel::updateOrCreate([
-                                'product_id' => $productObj->id,
-                            ], [
-                                'link' => trim($productLink),
-                            ]);
-                        }
-                    } elseif (isset($product->links)) {
-                        ProductLinkModel::where('product_id', $productObj->id)->forceDelete();
-
-                        foreach ($product->links->link as $link) {
-                            ProductLinkModel::create([
-                                'product_id' => $productObj->id,
-                                'link' => trim($link->href),
-                            ]);
-                        }
-                    }
+                    ProductModel::where('feed_hash', $feedHash)->whereNotIn('g_id', $products)->forceDelete();
                 }
-
-                ProductModel::where('feed_hash', $feedHash)->whereNotIn('g_id', $products)->forceDelete();
             }
 
             event(new UpdateProductsEvent());
