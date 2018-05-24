9cc50fd345bf753092a259743951575fe3ff413f
Konstantin Bukin <bukin@inetstudio.ru>
2018-05-25 00:28:58 +0300

    Change product link view

--- src/Providers/BladeServiceProvider.php
+++ src/Providers/BladeServiceProvider.php
@@ -18,7 +18,7 @@ class BladeServiceProvider extends ServiceProvider
             $params = explode(',', $expression, 2);
             $params = array_map('trim', $params, array_fill(0, count($params),"' \t\n\r\0\x0B"));
 
-            return view('admin.module.products::back.partials.content.product_link', [
+            return view('admin.module.products::front.partials.content.productLink_directive', [
                 'id' => $params[0],
                 'word' => $params[1],
             ]);
