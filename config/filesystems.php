42dd7fe1c9e51faf2f84244905db436e7ff3bfe0
Konstantin Bukin <bukin@inetstudio.ru>
2018-09-03 01:06:50 +0300

    Fix storage urls

--- config/filesystems.php
+++ config/filesystems.php
@@ -9,8 +9,8 @@ return [
 
     'products' => [
         'driver' => 'local',
-        'root' => storage_path('app/public/products/'),
-        'url' => env('APP_URL').'/storage/products/',
+        'root' => storage_path('app/public/products'),
+        'url' => env('APP_URL').'/storage/products',
         'visibility' => 'public',
     ],
 
