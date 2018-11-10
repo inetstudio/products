3ae51913e7d70a7f4567a3b938a7c876628538b0
Konstantin Bukin <bukin@inetstudio.ru>
2018-11-10 03:46:53 +0300

    Code reorganization

--- src/Services/Front/ProductsService.php
+++ src/Services/Front/ProductsService.php
@@ -3,75 +3,24 @@
 namespace InetStudio\Products\Services\Front;
 
 use Illuminate\Http\Request;
+use InetStudio\AdminPanel\Services\Front\BaseService;
 use InetStudio\Products\Contracts\Models\ProductModelContract;
+use InetStudio\Favorites\Services\Front\Traits\FavoritesServiceTrait;
 use InetStudio\Products\Contracts\Services\Front\ProductsServiceContract;
 
 /**
  * Class ProductsService.
  */
-class ProductsService implements ProductsServiceContract
+class ProductsService extends BaseService implements ProductsServiceContract
 {
-    /**
-     * @var
-     */
-    public $repository;
+    use FavoritesServiceTrait;
 
     /**
-     * EmbeddedProductsDataTableService constructor.
+     * ProductsService constructor.
      */
     public function __construct()
     {
-        $this->repository = app()->make('InetStudio\Products\Contracts\Repositories\ProductsRepositoryContract');
-    }
-
-    /**
-     * Возвращаем модель продукта.
-     *
-     * @param int $id
-     *
-     * @return mixed
-     */
-    public function getProduct(int $id)
-    {
-        return $this->repository->getItemByID($id);
-    }
-
-    /**
-     * Получаем объекты по списку id.
-     *
-     * @param array|int $ids
-     * @param array $params
-     *
-     * @return mixed
-     */
-    public function getProductsByIDs($ids, array $params = [])
-    {
-        return $this->repository->getItemsByIDs($ids, $params);
-    }
-
-    /**
-     * Получаем сохраненные объекты пользователя.
-     *
-     * @param mixed $userID
-     * @param array $params
-     *
-     * @return mixed
-     */
-    public function getProductsFavoritedByUser($userID, array $params = [])
-    {
-        return $this->repository->getItemsFavoritedByUser($userID, $params);
-    }
-
-    /**
-     * Получаем все объекты.
-     *
-     * @param array $params
-     *
-     * @return mixed
-     */
-    public function getAllProducts(array $params = [])
-    {
-        return $this->repository->getAllItems($params);
+        parent::__construct(app()->make('InetStudio\Products\Contracts\Repositories\ProductsRepositoryContract'));
     }
 
     /**
