09b6bff630ecd78ac167c315de41297adabedd5e
Konstantin Bukin <bukin@inetstudio.ru>
2019-03-27 06:03:02 +0300

    Fixes

--- resources/views/back/partials/datatables/shops.blade.php
+++ resources/views/back/partials/datatables/shops.blade.php
@@ -1,6 +1,6 @@
 <ul class="list-group clear-list m-t">
     @foreach ($shops as $shop => $products)
-        <li class="list-group-item-action">
+        <li class="list-group-item">
             <span class="label label-primary m-r-xs">{{ $products->sum('count') }}</span>{{ $shop }}
         </li>
     @endforeach
